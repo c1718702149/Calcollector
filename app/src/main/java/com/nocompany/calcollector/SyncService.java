4f24c8074cc2ec9d5d95d2fd35630a3990767313
c1718702149 <c1718702149@gmail.com>
2015-03-22 22:54:36 +0800

    Hello

---
+++ app/src/main/java/com/nocompany/calcollector/SyncService.java
@@ -0,0 +1,34 @@
+package com.nocompany.calcollector;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class SyncService extends Service {
+    private static final String TAG = "SyncService";
+    private static SyncAdapter sSyncAdapter = null;
+    private static final Object sSyncAdapterLock = new Object();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "Service created");
+        synchronized (sSyncAdapterLock) {
+            if (sSyncAdapter == null) {
+                sSyncAdapter = new SyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Service destroyed");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSyncAdapter.getSyncAdapterBinder();
+    }
+}
